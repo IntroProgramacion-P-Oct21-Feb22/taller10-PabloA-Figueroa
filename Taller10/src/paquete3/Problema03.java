7347f273a683735708e27efeb3ec93b321d60881
Pablo Figueroa <apfigueroa3@utpl.edu.ec>
2022-01-11 08:06:10 -0500

    Segundo Commit

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,5 +10,48 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
-    
+
+    public static void main(String[] args) {
+
+        String[][] notasCualitativas = new String[3][3];
+        double[][] notasCuantitativas = {{7.1, 8, 10}, {3, 7, 9}, {10, 9, 2.1}};
+
+        for (int fila = 0; fila < notasCuantitativas.length; fila++) {
+            for (int col = 0; col < notasCuantitativas[fila].length; col++) {
+                if (notasCuantitativas[fila][col] >= 0
+                        && notasCuantitativas[fila][col] <= 2.9) {
+                    notasCualitativas[fila][col] = "Insuficiente";
+                } else {
+                    if (notasCuantitativas[fila][col] >= 3
+                            && notasCuantitativas[fila][col] <= 4.9) {
+                        notasCualitativas[fila][col] = "Regular";
+                    } else {
+                        if (notasCuantitativas[fila][col] >= 5
+                                && notasCuantitativas[fila][col] <= 7.9) {
+                            notasCualitativas[fila][col] = "Buena";
+                        } else {
+                            if (notasCuantitativas[fila][col] >= 8
+                                    && notasCuantitativas[fila][col] <= 9.5) {
+                                notasCualitativas[fila][col] = "Muy Buena";
+                            } else {
+                                if (notasCuantitativas[fila][col] >= 9.6
+                                        && notasCuantitativas[fila][col] <= 10) {
+                                    notasCualitativas[fila][col] = "Sobresaliente";
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+        for (int fila = 0; fila < notasCuantitativas.length; fila++) {
+            for (int col = 0; col < notasCuantitativas[fila].length; col++) {
+                System.out.printf("%.2f --> %s\n", notasCuantitativas[fila][col],
+                        notasCualitativas[fila][col]);
+            }
+
+        }
+
+    }
 }
